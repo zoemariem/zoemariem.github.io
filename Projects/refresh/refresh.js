123e1eb29ba85ea94ace6d32d1af0aa46c8930cf
Zoe Michaux <zoe8034@gmail.com>
2022-02-09 12:54:52 -0600

    ading projects

---
+++ Projects/refresh/refresh.js
@@ -0,0 +1,11 @@
+		window.onload = rndmImg;
+		var img = ["Raccoon.jpg", "cat.jpg", "fox.jpeg", "panda.jpg", "sloth.jpg", "zebra.jpg"]
+		var cap = ["Raccoon", "Cat", "Fox", "Panda", "Sloth","Zebra"]
+
+	function rndmImg(){
+		var rndm = Math.floor(Math.random()* img.length);
+		document.getElementById('cluster').src = img[rndm];
+		document.getElementById("figcaption").innerHTML = cap[rndm]
+
+
+}
